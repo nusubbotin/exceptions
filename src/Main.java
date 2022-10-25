dde1078401501758fef3af29506d4e93a0e170ee
nusubbotin <nu.subbotin@ya.ru>
2022-10-25 22:51:42 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
 
+    private static void task1() {
+        Authorization auth = new Authorization("AlexPerm1988", "12345678!Q", "12345678!q");
+
+        try {
+            Data.checkAuthorization(auth);
+        }catch (WrongLoginException e) {
+            System.out.println(e.getMessage() + " login(" + e.getLogin() + ")");
+        }catch (WrongPasswordException e) {
+            System.out.println(e.getMessage() + " password(" + e.getPassword() + " confirmPassword(" + e.getConfirmPassword() + ")");
+        }finally {
+            System.out.println("Провели проверку авторизации для попытки доступа: " + auth);
+        }
     }
 }
