e29ca9df2d3357636ce15c2b6eb9faf4705a6e12
nusubbotin <nu.subbotin@ya.ru>
2022-10-25 23:15:21 +0300

    Дополнительная инициализация задания 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import car.Bus;
+import car.Car;
+import car.CargoCar;
+import car.Transport;
+
 public class Main {
     public static void main(String[] args) {
-        task1();
+        //task1();
+        task2();
+    }
+
+    private static void task2() {
+        System.out.println("Домашка 2");
+        Transport[] cars = new Transport [3];
+        cars[0] = new Car("лада", "гранта", 1.5f);
+        cars[1] = new Bus("Скания", "Скания модель", 5.6f);
+        cars[2] = new CargoCar("Самосвал", "Камаз", 6);
+
+        for (Transport car : cars) {
+            System.out.println(car);
+        }
+
+
     }
 
     private static void task1() {
